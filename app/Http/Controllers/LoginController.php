b81b0e894aab624ce44add0682d281212b89cc27
Tousif070 <tousifrafat@gmail.com>
2022-03-06 15:44:00 +0600

    Validation added for registration & login

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -10,6 +10,21 @@ class LoginController extends Controller
 {
     public function register(Request $request)
     {
+        $request->validate([
+            'name' => 'required | alpha',
+            'email' => 'required | email | unique:users,email',
+            'password' => 'required | alpha_num | confirmed'
+        ], [
+            'name.required' => 'Please enter your name !',
+            'name.alpha' => 'Only alphabets are allowed !',
+            'email.required' => 'Please enter your email !',
+            'email.email' => 'Please enter a valid email !',
+            'email.unique' => 'Email already exists !',
+            'password.required' => 'Please enter a password !',
+            'password.alpha_num' => 'Only alphabets & numbers are allowed !',
+            'password.confirmed' => 'Passwords do not match !'
+        ]);
+
         $user = new User();
 
         $user->name = $request->name;
@@ -25,11 +40,21 @@ class LoginController extends Controller
 
     public function login(Request $request)
     {
+        $request->validate([
+            'email' => 'required | email',
+            'password' => 'required | alpha_num'
+        ], [
+            'email.required' => 'Please enter your email !',
+            'email.email' => 'Please enter a valid email !',
+            'password.required' => 'Please enter your password !',
+            'password.alpha_num' => 'Only alphabets & numbers are allowed !'
+        ]);
+
         $user = User::where('email', '=', $request->email)->first();
 
         if($user == null || !Hash::check($request->password, $user->password))
         {
-            return response(['message' => 'Invalid Email/Password !'], 401);
+            return response(['message' => 'Invalid Email or Password !'], 401);
         }
 
         $user_token = $user->createToken(mt_rand(1, 1000000) . "_" . $user->email);
