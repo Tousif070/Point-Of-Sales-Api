5409cadb1001d119562dc37f694e0861bcfd9b5d
Tousif070 <tousifrafat@gmail.com>
2022-03-24 12:55:50 +0600

    Brand model & Product Category model added

--- database/migrations/2022_03_24_122525_create_products_table.php
+++ database/migrations/2022_03_24_122525_create_products_table.php
@@ -19,17 +19,36 @@ class CreateProductsTable extends Migration
 
             $table->string('name');
 
+
+            $table->integer('brand_id')->unsigned();
+
+            $table->foreign('brand_id')->references('id')->on('brands')->onDelete('cascade');
+
+
+            $table->integer('product_category_id')->unsigned();
+
+            $table->foreign('product_category_id')->references('id')->on('product_categories')->onDelete('cascade');
+
+
             $table->integer('sku')->unsigned()->nullable();
+
+            $table->string('image');
 
             $table->string('color');
 
-            $table->integer('ram')->unsigned();
+            $table->integer('ram')->unsigned()->nullable();
 
-            $table->integer('storage')->unsigned();
+            $table->integer('storage')->unsigned()->nullable();
 
             $table->string('condition');
 
-            $table->string('image');
+            $table->string('size')->nullable();
+
+            $table->string('wattage')->nullable();
+
+            $table->string('type')->nullable();
+
+            $table->string('length')->nullable();
 
             $table->timestamps();
 
